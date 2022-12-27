37e3d3c0a28c4f1484344888b16fa414949ff71d
pratik kagale <pratikkagale3333@gmail.com>
2022-12-27 19:49:32 +0530

    simple array program

---
+++ array/TestArrayApp.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TestArrayApp {
+    public static void main(String x[])
+    {
+        int a[] = new int[] {10,20,30,40,50};
+        int b[];
+        b=a;
+        b[1]=100;
+        System.out.println("Display the Array values: ");
+        for(int i=0; i<a.length; i++)
+        {
+            System.out.printf("%d\t", a[i]);
+        }
+
+    }
+    
+}
