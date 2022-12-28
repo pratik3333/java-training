465ff48bac52d4802a490263ce218fd58dbd18a1
pratik kagale <pratikkagale3333@gmail.com>
2022-12-28 20:29:53 +0530

    Showing Array using For Each (Something went wrong)

---
+++ array/PrintArray_USing_ForEach.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class PrintArray_USing_ForEach{
+    public static void main(String x[]) {
+
+        int[] a={10,20,30,40};
+        for(int i:a);
+        {
+            System.out.printf("Array values is ----> %d",i);
+        }
+
+
+        
+    }
+}
