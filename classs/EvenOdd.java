bf6e481750f9cd9f7456404ce79218f1d37ebeaf
pratik kagale <pratikkagale3333@gmail.com>
2023-01-11 11:12:12 +0530

    Checked Number is Even or Odd

---
+++ classs/EvenOdd.java
@@ -0,0 +1,15 @@
+class EvenOdd{
+
+    public static void main(String x[])
+    {
+        int no=12;
+        if(no%2==0)
+        {
+            System.out.println("Number is Even");
+        }
+        else
+        {
+            System.out.println("Number is Odd");
+        }
+    }
+}
