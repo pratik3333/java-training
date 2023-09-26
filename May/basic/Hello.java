71752aef57731cd6ccc2b5a577f6f986c5085fb1
pratik kagale <pratikkagale3333@gmail.com>
2023-09-26 15:57:59 +0530

    Java Basic code

---
+++ May/basic/Hello.java
@@ -0,0 +1,12 @@
+
+class Hello {
+
+    public static void main(String [] args)
+    {
+           System.out.println("Hello World");
+           int a=10;
+           int b=20;
+           int sum=a+b;
+           System.out.println(sum);
+    }
+}
