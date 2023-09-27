8c77afddfb17e02ed9cf8bb2a44385e7e986374e
pratik kagale <pratikkagale3333@gmail.com>
2023-09-27 23:12:47 +0530

    pattern

---
+++ May/basic/Patt.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Patt
+{
+	public static void main(String x[])
+	
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=5; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
