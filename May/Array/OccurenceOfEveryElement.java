ce3872f0aded7baa73910d1d1d0166f5e11dc8fc
pratik kagale <pratikkagale3333@gmail.com>
2023-10-03 20:44:56 +0530

    Array programs

---
+++ May/Array/OccurenceOfEveryElement.java
@@ -0,0 +1,48 @@
+
+import java.util.*;
+public class OccurenceOfEveryElement
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int i,j,count=0;
+		System.out.println("Enter the Size of Array");
+		
+		int size =sc.nextInt();	
+		int a[]=new int[size];
+		
+		System.out.println("Enter the Array values");
+		for(i=0; i<a.length; i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		
+		System.out.println("Display Array Values");
+		for(i=0; i<a.length; i++)
+		{
+			System.out.printf("%d\t",a[i]);
+		}
+		
+		System.out.printf("\nOccurence Of Every Element is: \n");
+		
+		for(i=0; i<a.length; i++)
+		{
+			
+			for(j=i; j<a.length; j++)
+			{
+				if(a[i]==a[j])
+				{
+					count++;
+					a[j]=-1;
+				}
+				
+			}
+			if(a[i]!=-1)
+			{
+			System.out.printf("%d--->%d\n",a[i],count);
+			count=0;
+			}
+		}
+	}
+}
