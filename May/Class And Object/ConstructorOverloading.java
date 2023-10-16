74e60d404da072d51d1b2d1cb838d6663ff45f0c
pratik kagale <pratikkagale3333@gmail.com>
2023-10-16 12:53:13 +0530

    Constructor OVerloading

---
+++ May/Class And Object/ConstructorOverloading.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Add
+{
+	Add(int a,int b)
+	{
+		System.out.printf("Addition of two Integer number is %d",a+b);
+	}
+	
+	Add(float x,float y)
+	{
+		System.out.printf("Addition of two Float number is %f",x+y);
+	}
+}
+
+public class ConstructorOverloading
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Integer values");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		System.out.println("Enter Float values");
+		float x=sc.nextFloat();
+		float y=sc.nextFloat();
+		
+		Add adi=new Add(a,b);
+		Add adf=new Add(x,y);
+	}
+}
