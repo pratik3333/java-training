79c1bbe9a10e0b9745983427b657422eab6144e5
pratik kagale <pratikkagale3333@gmail.com>
2023-10-21 14:16:36 +0530

    Array rotation

---
+++ May/Class And Object/RotateArrayFromN.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class Rotatation
+{
+	private int a[];
+	private int index;
+	
+	void setData(int a[],int index)
+	{
+		this.a=a;
+		this.index=index;
+	}
+	
+	void getData()
+	{
+		int temp;
+		for(int i=0; i<index; i++)
+		{
+			temp=a[0];
+			for(int j=0; j<a.length-1; j++)
+			{
+				a[j]=a[j+1];
+			}
+			a[a.length-1]=temp;
+		}
+		
+		for(int i=0; i<a.length; i++)
+		{
+			System.out.printf("%d\t",a[i]);
+		}
+	}
+}
+
+public class RotateArrayFromN
+{
+	public static void main(String x[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[5];
+		
+		System.out.println("Enter array values");
+		for(int i=0; i<a.length; i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		
+		System.out.println("Enter index");
+		int index=sc.nextInt();
+		
+		Rotatation ro = new Rotatation();
+		ro.setData(a,index);
+		ro.getData();
+	}
+}
