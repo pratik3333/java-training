9f17d28cf325ddfc80dd237dc9c36bb10557c6c6
pratik kagale <pratikkagale3333@gmail.com>
2024-02-16 13:37:25 +0530

    practice

---
+++ May/basic/EvenOddNum.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class EvenOddNum
+{
+	public static void main(String[] args)
+	{
+		Scanner sc =new Scanner(System.in);
+		//Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter number:- ");
+		int num =sc.nextInt();
+		
+		if(num%2==0)
+		{
+			System.out.println("Number is even");
+		}
+		else
+		{
+			System.out.println("Number is Odd");
+		}
+	}
+}
---
+++ May/basic/HelloWord.java
@@ -0,0 +1,16 @@
+// public class HelloWord {
+    
+//     public static void main (String[] args)
+//     {
+
+//         System.out.println("Hello Word !!!");
+//     }
+// }
+
+
+public class HelloWord {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
