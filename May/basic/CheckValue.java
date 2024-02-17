d355b862b337403169ba677ad4853f61e351973b
pratik kagale <pratikkagale3333@gmail.com>
2024-02-17 17:16:37 +0530

    assignment operator

---
+++ May/basic/CheckValue.java
@@ -0,0 +1,10 @@
+public class CheckValue {
+
+    public static void main(String[] args) {
+        int x=6;
+        int y=9;
+
+        boolean result=x<y;
+        System.out.println(result);
+    }
+}
