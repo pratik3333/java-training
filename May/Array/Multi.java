bec77e8cf6732273ebdb1bd2deaf628da569c25f
pratik kagale <pratikkagale3333@gmail.com>
2024-02-19 18:09:16 +0530

    Array

--- May/Array/Multi.java
+++ May/Array/Multi.java
@@ -18,7 +18,7 @@ public class Multi {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
             
-            System.out.printf("%d ",arr[i][j]);
+            System.out.print(arr[i][j]+ " ");
             }
             System.out.println();
         }
