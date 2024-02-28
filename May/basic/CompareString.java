c423b632a9a849b0a651c48f1c8545733fe5df0d
pratik kagale <pratikkagale3333@gmail.com>
2024-02-28 17:57:50 +0530

    issue occure when lengths are not same

---
+++ May/basic/CompareString.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class CompareString {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two name");
+        String a=sc.next();
+        String b=sc.next();
+
+        int flag=1;
+        
+        if (a.length()==b.length()) {
+            for (int i = 0; i < a.length(); i++) {
+                for (int j = i; j == i; j++) {
+                    if (a.charAt(i)==b.charAt(j)) {
+                        flag=1;
+
+                    }
+                    else
+                    {
+                        flag=0;
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            System.out.println("Names are not same");
+        
+        }
+
+        if(flag==1 )
+        {
+            System.out.println("names are same");
+        }
+        else{
+            System.out.println("names are not same");
+            
+        }
+    }
+}
