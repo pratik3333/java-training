7199c0c27bd3be6939b3a48ad584f31e51d0d007
pratik kagale <pratikkagale3333@gmail.com>
2024-03-01 10:44:33 +0530

    even length words in string

--- old-Data/basic/StringEven_Length.java
+++ old-Data/basic/StringEven_Length.java
@@ -2,6 +2,12 @@ public class StringEven_Length {
     public static void main(String[] args) {
         String str = "This is a sample string with even and odd length words";
         
+        // String s[]=str.split(" ");
+        // for (int i = 0; i < s.length; i++) {
+            
+        //     System.out.println(s[i]);
+        // }
+        
         int start = 0;
         for (int i = 0; i <= str.length(); i++) {
             if (i == str.length() || str.charAt(i) == ' ') 
