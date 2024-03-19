ed5ea5ab393f04ebafa46670a122750e062303b7
pratik kagale <pratikkagale3333@gmail.com>
2024-03-19 11:26:17 +0530

    Eclipse IDE files

---
+++ May/POJO/trial/src/techhub/org/Demo.java
@@ -0,0 +1,14 @@
+package techhub.org;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word");
+		
+		Demo2 dm=new Demo2();
+		dm.show();
+
+	}
+
+}
