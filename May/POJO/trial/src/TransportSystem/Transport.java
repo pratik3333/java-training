37128e4fa5f4c69a2d989e13594807bcaf0017c0
pratik kagale <pratikkagale3333@gmail.com>
2024-03-21 15:34:43 +0530

    Transport App

--- May/POJO/trial/src/TransportSystem/Transport.java
+++ May/POJO/trial/src/TransportSystem/Transport.java
@@ -1,5 +1,9 @@
 package TransportSystem;
 
+import TransportSystem.Bus;
+import TransportSystem.Truck;
+import TransportSystem.Vehical;
+
 class Vehical{
 	
 	protected int model;
@@ -62,9 +66,40 @@ class Truck extends Vehical{
 	
 }
 
-//class Bus extends Vehical{
-//	
-//}
+class Bus extends Vehical{
+	
+	
+	protected int npassenger;
+	protected int time;
+	public Bus(int model, int regno, int vspeed, int fcapacity, int fconsumption,int npassenger)
+	{
+		super( model,regno,vspeed,fcapacity,fconsumption);
+		this.npassenger=npassenger;
+	}
+	
+	
+//	public Bus(int i, int j, int k, int l, int m, int n) {
+//		// TODO Auto-generated constructor stub
+//	}
+
+
+	public void display()
+	{
+		
+		System.out.println("Truck model number is "+ model +"\n" + "Truck Registration number is "+ regno +"\n" +
+				"Truck speed is " + vspeed + "\n" +"Truck fuelConsumption is " + fconsumption + "\n" + "Total number of passenger is " + npassenger);
+		
+	}
+	
+	public void distanceCovered(int time)
+	{
+		this.time=time;
+		
+		int dis=vspeed/time;
+		System.out.println("The Bus covered distance is " + dis);
+		
+	}
+}
 
 
 
@@ -78,7 +113,12 @@ public class Transport {
 		Truck truck =new Truck(1001, 111, 10,30, 3, 30);
 		truck.display();
 		truck.fuelNeeded(30);
+		System.out.println("----------------***-------------------");
+		
 
+		Bus bus= new Bus(1002, 222,100,30,20,333);
+		bus.display();
+		bus.distanceCovered(5);
 	}
 
 }
