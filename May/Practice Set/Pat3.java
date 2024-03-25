6f90fab02a26c63a657a46baaf62b2c01b9fcbec
pratik kagale <pratikkagale3333@gmail.com>
2024-03-25 14:44:45 +0530

    Number increasing pyramid pattern

---
+++ May/Practice Set/Pat3.java
@@ -0,0 +1,15 @@
+
+public class Pat3
+{
+	public static void main(String args[])
+	{
+		for(int i=1; i<=4; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
