872e0bc4da73dc4fac30a431d460a482d043211d
pratik kagale <pratikkagale3333@gmail.com>
2024-03-25 13:38:58 +0530

    pattern in java

---
+++ May/Practice Set/Pattern1.java
@@ -0,0 +1,21 @@
+
+
+public class Pattern1
+{
+	public static void main(String args[])
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=5; j++)
+			{
+				if(i==1 || j==1 || i==5 || j==5)
+				{
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+			
+		}
+		
+	}
+}
