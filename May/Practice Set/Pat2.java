812419b194dbddd860e88142cf90b4bf602d81c2
pratik kagale <pratikkagale3333@gmail.com>
2024-03-25 14:30:43 +0530

    pattern printing

--- May/Practice Set/Pat2.java
+++ May/Practice Set/Pat2.java
@@ -10,24 +10,24 @@ public class Pat2
 			for(int j=1; j<=11; j++)
 			{
 				if(f==true)
-				{
-				 if(j<=5+i && j>=7-i)
-					{
-					System.out.print(i);
-					f=false;
-					}
-				else
-				{
-					System.out.print(" ");
+				 {
+				     if(j<=5+i && j>=7-i)
+				     	{
+					       System.out.print(i);
+					       f=false;
+					    }
+				    else
+				        {
+					       System.out.print(" ");
 				
-				}
-				}
+				        }
+				  }
 				else
-				{
+				 {
 					System.out.print(" ");
 					f=true;
 					
-				}
+				 }
 				
 				
 			}
