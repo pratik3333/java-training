c68fc81efbf8f092fdfbb1b50f3d0226c6aaacb6
pratik kagale <pratikkagale3333@gmail.com>
2024-04-09 17:38:20 +0530

    Compare two string

---
+++ May/StringCode/src/StringApp/CompareTwoString.java
@@ -0,0 +1,30 @@
+package StringApp;
+import java.util.*;
+
+public class CompareTwoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter First String:--");
+		String str=sc.nextLine();
+		
+		System.out.println("Enter Second string:--");
+		String str1=sc.nextLine();
+		
+		int p=str.compareTo(str1);
+		
+		if(p!=0)
+		{
+			System.out.println("Strings are not equal");
+		}
+		else
+		{
+			System.out.println("Strings are equal");
+		}
+		
+	}
+
+}
