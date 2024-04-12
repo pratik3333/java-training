940bb67d2379391af10282a9caaa40482212b86d
pratik kagale <pratikkagale3333@gmail.com>
2024-04-12 15:04:57 +0530

    Toggle case example

--- May/StringCode/src/StringApp/ToggleCaseString.java
+++ May/StringCode/src/StringApp/ToggleCaseString.java
@@ -13,32 +13,28 @@ public class ToggleCaseString {
 		System.out.println("Enter string here...");
 		String str=sc.nextLine();
 		
-		String str1;
-		//char ch1;
-		for(int i=0; i<str.length(); i++)
-		{
-			char ch=str.charAt(i);
-			
-			if(ch>='a' && ch<='z')
-			{
-			  int ss=	ch-48 +32;
-	
-				
-			 	//int ch1=parseInt(ch);
-				//int ch1=parseInt(ch);
-				
-				//char chh=ParseString(ch1);
-			 	
-			 	
-			 	System.out.println("After Conversion "+ ss);
-				
-				
-				
-	
-			}
 		
+		char ch[]=str.toCharArray();
+		
+		//char ch1;
+		for(int i=0; i<ch.length; i++)
+		{
+			
+			if(ch[i]>='a' && ch[i]<='z' && ch[i]!=' ')
+			{
+			ch[i]=(char) (ch[i]-32);
+			 }
+			else if(ch[i]>='A' && ch[i]<='Z' && ch[i]!=' ')
+			{
+				ch[i]=(char) (ch[i]+32);
+				
+			}
 			
 		}
+		String str1=new String(ch);
+		System.out.println(str1);
+		
+		
 		
 	}
 
