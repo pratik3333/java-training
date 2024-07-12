97d686f70b62c6d60d37324a480591e914bda798
pratik kagale <pratikkagale3333@gmail.com>
2024-07-12 19:03:52 +0530

    reading file using FileReader

---
+++ May/Practice Set/Practice/src/FileHandling/org/ReadDemoFile.java
@@ -0,0 +1,21 @@
+package FileHandling.org;
+import java.io.*;
+
+public class ReadDemoFile {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		FileReader fr=new FileReader("H:\\Java training\\May\\Practice Set\\Practice\\src\\FileHandling\\org\\NewFolder\\demo2.txt");
+		
+		int data;
+		
+		while((data=fr.read())!=-1)
+		{
+			char ch=(char)data;
+			System.out.println(ch);
+			Thread.sleep(100);
+		}
+
+	}
+
+}
