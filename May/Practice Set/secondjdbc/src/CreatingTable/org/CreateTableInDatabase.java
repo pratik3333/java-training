be6613b8a4cfdca65ecbb91730e04329236db192
pratik kagale <pratikkagale3333@gmail.com>
2024-08-02 23:09:54 +0530

    created table in database

---
+++ May/Practice Set/secondjdbc/src/CreatingTable/org/CreateTableInDatabase.java
@@ -0,0 +1,33 @@
+package CreatingTable.org;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateTableInDatabase {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/techhub","root","root");
+		
+		try
+		{
+		String q="create table table1 (tid int(5) primary key auto_increment, tname varchar(20) not null)";
+		
+		Statement stmt=conn.createStatement();
+		
+		stmt.executeUpdate(q);
+		
+		System.out.println("table created Succesfully...");
+		conn.close();
+		
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+}
